b49daf5412dcef517f340aa627ce3d8fd5f55e55
takeabyte <takeabyte@users.noreply.github.com>
2022-02-15 23:52:49 +0100

    send all hotspot results to influx db

--- nodered_reformat_json.js
+++ nodered_reformat_json.js
@@ -1,22 +1,28 @@
-var thing = [{
-    reported_at: msg.payload.reported_at,
-    hotspot_name: msg.payload.hotspot_name,
-    rssi: msg.payload.rssi,
-    snr: msg.payload.snr,
-    spreading: msg.payload.spreading,
-    battery: msg.payload.battery,
-    lat: msg.payload.lat,
-    lon: msg.payload.lon,
-    speed: msg.payload.speed,
-    sats: msg.payload.sats,
-},
+for (var i = 0; i < msg.payload.hotspots.length-1; i++) {
+ 
+    var thing = [
+        {
+            reported_at: msg.payload.reported_at,
+            hotspot_name: msg.payload.hotspots[i].hotspot_name,
+            rssi: msg.payload.hotspots[i].rssi,
+            snr: msg.payload.hotspots[i].snr,
+            spreading: msg.payload.hotspots[i].spreading,
+            battery: msg.payload.battery,
+            lat: msg.payload.lat,
+            lon: msg.payload.lon,
+            speed: msg.payload.speed,
+            sats: msg.payload.sats,
+        },
 
-{
-  geohash:msg.payload.geohash,
-  name:msg.payload.name
+        {
+          geohash:msg.payload.geohash,
+          name:msg.payload.name
+        }
+    ]
+    
+    var newMsg = {};
+    newMsg.payload = thing;
+    node.send(newMsg);
 }
 
-]
-
-msg.payload = thing;
-return msg;
+return null;
