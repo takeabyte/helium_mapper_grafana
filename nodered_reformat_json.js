eb9be078b906e3cce51a4a300683dd48b4a2a9ae
takeabyte <takeabyte@users.noreply.github.com>
2022-02-18 01:04:01 +0100

    updated nodered to calculate distance in km
    takes gps data from mapper and hotspot to calculate distance in km

--- nodered_reformat_json.js
+++ nodered_reformat_json.js
@@ -1,5 +1,23 @@
+function calcCrow(lat1, lon1, lat2, lon2) 
+    {
+      var R = 6371; // km
+      var dLat = toRad(lat2-lat1);
+      var dLon = toRad(lon2-lon1);
+      var lat1 = toRad(lat1);
+      var lat2 = toRad(lat2);
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c;
+      return d;
+    }
+    // Converts numeric degrees to radians
+    function toRad(Value) 
+    {
+        return Value * Math.PI / 180;
+    }
+
 for (var i = 0; i < msg.payload.hotspots.length-1; i++) {
- 
     var thing = [
         {
             reported_at: msg.payload.reported_at,
@@ -15,6 +33,7 @@ for (var i = 0; i < msg.payload.hotspots.length-1; i++) {
             lon: msg.payload.lon,
             speed: msg.payload.speed,
             sats: msg.payload.sats,
+            distance_km: msg.payload["distance"]= Number(calcCrow(msg.payload.lat,msg.payload.lon,msg.payload.hotspots[i].hslat,msg.payload.hotspots[i].hslong).toFixed(1)),
         },
 
         {
